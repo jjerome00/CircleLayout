52900a7f08e85d0183f5761e22c032a0d66042af
Jason Jerome <jjerome00@users.noreply.github.com>
2020-01-09 16:36:58 -0800

    Add custom view (#1)

--- app/src/main/java/com/jasonjerome/circlelayout/MainActivity.kt
+++ app/src/main/java/com/jasonjerome/circlelayout/MainActivity.kt
@@ -1,12 +1,45 @@
 package com.jasonjerome.circlelayout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.jasonjerome.circlelayout.util.RandomColors
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.round_image.view.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val randomColors = RandomColors()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        circleLayout.sectionLayout = R.layout.round_image
+
+        lessButton.setOnClickListener {
+            setCircleSections(circleLayout.sectionCount - 1)
+        }
+
+        moreButton.setOnClickListener {
+            setCircleSections(circleLayout.sectionCount + 1)
+        }
+
+        setCircleColors()
     }
+
+    private fun setCircleColors() {
+        circleLayout.sectionList.forEach { view ->
+            val tag = view.tag
+            if (tag == null) {
+                view.image_id.setBackgroundColor(randomColors.color)
+                view.tag = true
+            }
+        }
+    }
+
+    private fun setCircleSections(newSectionCount: Int) {
+        circleLayout.sectionCount = newSectionCount
+        setCircleColors()
+    }
+
 }
---
+++ app/src/main/java/com/jasonjerome/circlelayout/view/CircularIconLayout.kt
@@ -0,0 +1,130 @@
+package com.jasonjerome.circlelayout.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.annotation.LayoutRes
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import com.jasonjerome.circlelayout.R
+import kotlin.math.roundToInt
+
+class CircularIconLayout : ConstraintLayout {
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int)
+            : super(context, attrs, defStyleAttr) {
+        init(attrs)
+    }
+
+    companion object {
+        private const val MIN_SECTIONS = 0
+        private const val CIRCLE_RADIUS = 120
+    }
+
+    private var circleRadius = CIRCLE_RADIUS
+    private lateinit var centerView: TextView
+
+    val sectionList = mutableListOf<View>()
+
+    var sectionCount = MIN_SECTIONS
+        set(value) {
+            field = if (value < MIN_SECTIONS) MIN_SECTIONS else value
+            setupIconRestraints()
+        }
+
+    @LayoutRes
+    var sectionLayout: Int = R.layout.round_image
+        set(value) {
+            field = value
+            setupIconRestraints()
+        }
+
+    private fun init(attrs: AttributeSet?) {
+        var requestedSectionCount = MIN_SECTIONS
+
+        // "center view" - all views will be constrained to this one.
+        centerView = TextView(context).apply {
+            id = R.id.CircularIconCenter
+            layoutParams = LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+            visibility = View.INVISIBLE
+            text = "*"
+        }
+        this.addView(centerView)
+
+        attrs?.let {
+            val styleValues = context.obtainStyledAttributes(it, R.styleable.CircularIconLayout, 0, 0)
+            requestedSectionCount = styleValues.getInt(R.styleable.CircularIconLayout_sections, MIN_SECTIONS)
+            circleRadius = styleValues.getInt(R.styleable.CircularIconLayout_radius, CIRCLE_RADIUS)
+            styleValues.recycle()
+        }
+
+        sectionCount = requestedSectionCount
+    }
+
+    /**
+     * Convert an Int (pixel) value to density pixels (higher res screens have more pixels)
+     */
+    private fun Int.toDensityPixels(): Int {
+        val density = context.resources.displayMetrics.density
+        return (this * density).roundToInt()
+    }
+
+    /**
+     * Updates views based on the sectionCount.
+     * If the sectionCount changes, we need to either add/remove views appropriately
+     * - `sectionLayout` is the layout file used for each section's view to be displayed
+     */
+    private fun updateSections() {
+        if (sectionCount > sectionList.size) {
+            val newSections = sectionCount - sectionList.size
+            repeat(newSections) {
+                val section = View.inflate(context, sectionLayout, null).apply {
+                    id = View.generateViewId()
+                }
+                sectionList.add(section)
+                this.addView(section)
+            }
+        } else if (sectionCount < sectionList.size) {
+            val removeSections = sectionList.size - sectionCount
+            repeat(removeSections) {
+                this.removeView(sectionList.last())
+                sectionList.remove(sectionList.last())
+            }
+        } //else: same amount of sections, do nothing
+    }
+
+    /**
+     * 1. Constrain `centerView` to the middle of the (parent) view
+     * 2. For each (requested) section, constrain it to `centerView`
+     */
+    private fun setupIconRestraints() {
+        // add/remove sections based on sectionCount
+        updateSections()
+
+        // constrain sections to the centerView
+        val constraintSet = ConstraintSet()
+        constraintSet.clone(this)
+
+        // constrain centerView to the middle of our (parent) view
+        constraintSet.connect(centerView.id, ConstraintSet.TOP, this.id, ConstraintSet.TOP)
+        constraintSet.connect(centerView.id, ConstraintSet.BOTTOM, this.id, ConstraintSet.BOTTOM)
+        constraintSet.connect(centerView.id, ConstraintSet.START, this.id, ConstraintSet.START)
+        constraintSet.connect(centerView.id, ConstraintSet.END, this.id, ConstraintSet.END)
+
+        val distanceFromCenter = circleRadius.toDensityPixels()
+        val segmentDegrees: Float = 360.0f / sectionCount.toFloat()
+        var angle: Float = segmentDegrees / 2.0f
+
+        // constrain each section to the centerView
+        sectionList.forEach { section ->
+            constraintSet.constrainCircle(section.id, centerView.id, distanceFromCenter, angle)
+            angle += segmentDegrees
+        }
+
+        constraintSet.applyTo(this)
+    }
+
+}
